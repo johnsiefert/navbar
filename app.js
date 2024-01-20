e84a17dea43cab33b69ce0cf14bcd8908317ba1b
John <johnsief12@gmail.com>
2024-01-20 12:59:10 -0500

    added add/remove if conditional

--- app.js
+++ app.js
@@ -3,3 +3,17 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+const links = document.querySelector('.links');
+const toggle = document.querySelector('.nav-toggle');
+
+function toggleClick(){
+    let showLinks = links.classList;
+    console.log(showLinks)
+    if(showLinks.contains('show-links')) {
+        links.classList.remove('show-links');
+    }else{
+        links.classList.add('show-links');
+    }
+}
+
+toggle.addEventListener('click',toggleClick)
